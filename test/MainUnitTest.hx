8df5bd8566ba80de0095816b3f3c533b1e5bae9c
ali_o_kan <laurent@geturl.net>
2016-07-07 14:14:16 +0200

    add php target for travis

--- test/MainUnitTest.hx
+++ test/MainUnitTest.hx
@@ -2,13 +2,11 @@ package;
 
 import hex.HexUnitSuite;
 import hex.unittest.runner.ExMachinaUnitCore;
-import hex.unittest.runner.TestRunner;
-
-#if flash
-import flash.Lib;
 import hex.unittest.notifier.TraceNotifier;
-#else
+#if js
 import hex.unittest.notifier.ConsoleNotifier;
+#elseif flash
+import flash.Lib;
 #end
 
 /**
@@ -22,10 +20,11 @@ class MainUnitTest
 		var emu = new ExMachinaUnitCore();
         
 		#if flash
-		TestRunner.RENDER_DELAY = 0;
 		emu.addListener( new TraceNotifier( Lib.current.loaderInfo ) );
-		#else
+		#elseif js
 		emu.addListener( new ConsoleNotifier( false ) );
+		#else
+		emu.addListener( new TraceNotifier( false ) );
 		#end
 		
         emu.addTest( HexUnitSuite );
