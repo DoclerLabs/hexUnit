6bd12395a6604b901ca79a9f34082cc32acb1352
Laurent Deketelaere <Laurent.Deketelaere@doclerholding.com>
2018-04-30 10:19:29 +0200

    use Null<T> instead of Dynamic for isNull and isNotNull

--- src/hex/unittest/assertion/Assert.hx
+++ src/hex/unittest/assertion/Assert.hx
@@ -97,7 +97,7 @@ class Assert
     /**
      * Asserts that 'value' is null
      */
-    public static function isNull( value : Dynamic, userMessage : String = "", ?posInfos : PosInfos ) : Void
+    public static function isNull<T>( value : Null<T>, userMessage : String = "", ?posInfos : PosInfos ) : Void
     {
         Assert._LOG_ASSERT( userMessage );
 
@@ -110,7 +110,7 @@ class Assert
     /**
      * Asserts that 'value' is not null
      */
-    public static function isNotNull( value : Dynamic, userMessage : String = "", ?posInfos : PosInfos ) : Void
+    public static function isNotNull<T>( value : Null<T>, userMessage : String = "", ?posInfos : PosInfos ) : Void
     {
         Assert._LOG_ASSERT( userMessage );
 
